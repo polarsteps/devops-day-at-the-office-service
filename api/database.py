13ca74d710ef250264ace566195661a0e26c86cd
Anand Tripathi <anand.tripathi507@gmail.com>
2023-10-24 18:03:14 +0200

    Added scripts and typer for init db

--- api/database.py
+++ api/database.py
@@ -2,17 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 
 from api.config import settings
-from api.models import BaseModel
 
 engine = create_engine(settings.DATABASE_URL)
 DBSession = sessionmaker(bind=engine)
-
-
-def init_db(db_url: str) -> Session:
-    engine = create_engine(db_url)
-    DBSession = sessionmaker(bind=engine)
-    BaseModel.metadata.create_all(engine)
-    return DBSession
 
 
 def get_db() -> Session:
