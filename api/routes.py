225a773d63d92055a3c00e12951b0dd598155f0c
Ivan Kiselev <kiselev_ivan@pm.me>
2023-12-07 10:30:19 +0100

    basic CI setup

--- api/routes.py
+++ api/routes.py
@@ -12,7 +12,7 @@ from api.schemas import TripCreate, TripInDB, TripUpdate
 router = APIRouter(prefix="/trips")
 
 
-@router.post("/")
+# @router.post("/")
 async def create_trip(trip_data: TripCreate, db: Session = Depends(get_db)) -> TripInDB:
     params = trip_data.model_dump()
     if params["start_time"].date() == datetime.now(timezone.utc).date():
@@ -23,7 +23,7 @@ async def create_trip(trip_data: TripCreate, db: Session = Depends(get_db)) -> T
     return trip
 
 
-@router.get("/{trip_id}")
+# @router.get("/{trip_id}")
 async def get_trip(trip_id: int, db: Session = Depends(get_db)) -> dict[str, Any]:
     trip = db.execute(select(Trip).where(Trip.id == trip_id)).scalar_one_or_none()
     if trip is None:
@@ -37,7 +37,7 @@ async def get_trip(trip_id: int, db: Session = Depends(get_db)) -> dict[str, Any
     }
 
 
-@router.post("/{trip_id}")
+# @router.post("/{trip_id}")
 async def update_trip(
     trip_id: int, trip_data: TripUpdate, db: Session = Depends(get_db)
 ) -> TripInDB:
